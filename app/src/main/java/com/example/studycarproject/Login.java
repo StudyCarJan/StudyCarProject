2abe64bf1a1bb22640245b9932c914a48e1b9078
StudyCarMaxi <maxihaag.mh@gmail.com>
2019-02-07 12:43:45 +0100

    Actvity: LogIn und Registrierung

---
+++ app/src/main/java/com/example/studycarproject/Login.java
@@ -0,0 +1,28 @@
+package com.example.studycarproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnRegistrieren;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnRegistrieren = (Button) findViewById(R.id.btnRegistrieren);
+        btnRegistrieren.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent registrieren = new Intent(this,Registrierung.class);
+        startActivity(registrieren);
+    }
+}
