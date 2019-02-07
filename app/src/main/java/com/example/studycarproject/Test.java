4d0cd9a2fb5f79a8cac3a2cda7d70258c68b465a
StudyCarJan <hellmuthjan@gmail.com>
2019-02-07 12:29:00 +0100

    Neue Activity

--- app/src/main/java/com/example/studycarproject/Test.java
+++ app/src/main/java/com/example/studycarproject/Test.java
@@ -24,7 +24,7 @@ public class Test extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        //test
+        //test juhu
 
         //WASLUSTIGES
     }
