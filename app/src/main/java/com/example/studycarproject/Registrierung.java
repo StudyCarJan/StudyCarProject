6ec9bbb0c6e48394963e821294654604b8dc64d0
StudyCarJan <hellmuthjan@gmail.com>
2019-02-08 15:33:06 +0100

    Button-Funktion von Registrierung zu Bestaetigung

--- app/src/main/java/com/example/studycarproject/Registrierung.java
+++ app/src/main/java/com/example/studycarproject/Registrierung.java
@@ -1,13 +1,29 @@
 package com.example.studycarproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Registrierung extends AppCompatActivity {
+public class Registrierung extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnRegistrierungAbschicken;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrierung);
+
+        btnRegistrierungAbschicken = findViewById(R.id.btnRegistrierungAbschicken);
+        btnRegistrierungAbschicken.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent registrieren = new Intent(this,Registrierung.class);
+        startActivity(registrieren);
+
     }
 }
