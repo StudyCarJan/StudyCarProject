130d911d0023efe14719925dd87553b2f4e5cdf6
StudyCarJan <hellmuthjan@gmail.com>
2019-02-08 16:32:57 +0100

    Weitere Button-Verbindungen

--- app/src/main/java/com/example/studycarproject/Bestaetigung.java
+++ app/src/main/java/com/example/studycarproject/Bestaetigung.java
@@ -1,13 +1,27 @@
 package com.example.studycarproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Bestaetigung extends AppCompatActivity {
+public class Bestaetigung extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAbschicken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bestaetigung);
+
+        btnAbschicken = findViewById(R.id.btnAbschicken);
+        btnAbschicken.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent abschicken = new Intent(this, Startseite.class);
+        startActivity(abschicken);
     }
 }
