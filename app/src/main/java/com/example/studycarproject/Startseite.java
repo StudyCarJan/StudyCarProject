d4e8cc29676c3e74c3761c7f783edcf1dd2922e6
StudyCarJan <hellmuthjan@gmail.com>
2019-02-08 17:04:09 +0100

    FahgemeinschaftUebersicht und Button-Funktionen

--- app/src/main/java/com/example/studycarproject/Startseite.java
+++ app/src/main/java/com/example/studycarproject/Startseite.java
@@ -1,13 +1,26 @@
 package com.example.studycarproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Startseite extends AppCompatActivity {
+public class Startseite extends AppCompatActivity implements View.OnClickListener {
 
+    Button btnFahrgemeinschaft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startseite);
+
+        btnFahrgemeinschaft = findViewById(R.id.btnFahrgemeinschaft);
+        btnFahrgemeinschaft.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent fahrgemeinschaftSuchen = new Intent(this, FahrgemeinschaftUebersicht.class);
+        startActivity(fahrgemeinschaftSuchen);
     }
 }
