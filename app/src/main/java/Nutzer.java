3ad767a428965b76bad8308a210325ac0b7a5882
StudyCarJan <hellmuthjan@gmail.com>
2019-02-10 12:28:48 +0100

    Nutzer-Klasse-Erweiterung

--- app/src/main/java/Nutzer.java
+++ app/src/main/java/Nutzer.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Nutzer {
 
     private int nutzerId;
+    private static int counter = 1;
 
     private String nachname;
     private String vorname;
@@ -16,7 +17,7 @@ public class Nutzer {
 
     private Fahrgemeinschaft fahrgemeinschaft;
 
-    private int bestaetigungscode;
+    private String bestaetigungscode;
 
     public Nutzer(String vname, String nname, String mail, String pwort, int plz, String ort,
                   String studort) {
@@ -28,17 +29,23 @@ public class Nutzer {
         passwort = pwort;
         studienort = studort;
 
-        //nutzerId = ;
+        nutzerId = counter;
+        counter++;
 
         Random a = new Random();
         bestaetigungscode = erstelleCode();
     }
 
-    private int erstelleCode() {
+    private String erstelleCode() {
         Random a = new Random();
         int zahl = a.nextInt(99999);
+        String s = String.valueOf(zahl);
         int digits = String.valueOf(zahl).length();
-        //TODO fertig schreiben
-        return zahl;
+
+        while(s.length() < 5-digits) {
+            s = "0" + s;
+        }
+
+        return s;
     }
 }
