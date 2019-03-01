6a346f213453e46702737abf81baa7d9f8b31da6
StudyCarJan <hellmuthjan@gmail.com>
2019-03-01 13:31:38 +0100

    Finale Anpassungen

--- app/src/main/java/com/example/studycarproject/Route.java
+++ app/src/main/java/com/example/studycarproject/Route.java
@@ -3,19 +3,19 @@ package com.example.studycarproject;
 public class Route {
     private static int counter;
 
-    private int routenid;
+    private int routenId;
     private String start;
     private String ende;
 
     public Route(String start, String ende){
-        this.routenid = counter;
+        this.routenId = counter;
         this.start = start;
         this.ende = ende;
 
         counter++;
     }
 
-    public int getRoutenID() {return this.routenid; }
+    public int getRoutenID() {return this.routenId; }
     public String getStart() { return this.start; }
     public String getEnde() {return this.ende; }
 
